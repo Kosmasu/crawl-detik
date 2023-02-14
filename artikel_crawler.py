a5b5171cd4ec54ca7660aa87fecf77084f9a8763
Kenny <ken_link26@yahoo.com>
2023-02-14 16:02:06 +0700

    error handling

--- artikel_crawler.py
+++ artikel_crawler.py
@@ -15,16 +15,21 @@ def crawl(url):
   # del f
 
   # parsing header
-  header = re.search(r"<div class=\"detail__header\">([\s\S\n]+?)detail__date([\s\S\n]+?)</div>", article).group()
-  page["title"] = re.search(r"<h1 class=\"detail__title\">([\s\S\n]+?)</h1>", header).group(1).strip()
-  page["author"] = re.search(r"<div class=\"detail__author\">([\s\S\n]+?)<span", header).group(1).strip().removesuffix("-").rstrip()
-  page["date"] = re.search(r"<div class=\"detail__date\">([\s\S\n]+?)</div>", header).group(1).strip()
-  del header
+  try:
+    header = re.search(r"<div class=\"detail__header\">([\s\S\n]+?)detail__date([\s\S\n]+?)</div>", article).group()
+    page["title"] = re.search(r"<h1 class=\"detail__title\">([\s\S\n]+?)</h1>", header).group(1).strip()
+    page["author"] = re.search(r"<div class=\"detail__author\">([\s\S\n]+?)<span", header).group(1).strip().removesuffix("-").rstrip()
+    page["date"] = re.search(r"<div class=\"detail__date\">([\s\S\n]+?)</div>", header).group(1).strip()
+    del header
+  except:
+    print("\terror crawl header!\n url:", url)
 
   # parsing content
-  body = re.search(r"<div([\s\S\n]+?)id=\"detikdetailtext\">([\s\S\n]+?)detail__body-tag([\s\S\n]+?)</div>", article).group(2)
-  page["contents"] = re.findall(r"<p>([^<>]+?)</p>", body)
-  del body
-
+  try:
+    body = re.search(r"<div([\s\S\n]+?)id=\"detikdetailtext\">([\s\S\n]+?)detail__body-tag([\s\S\n]+?)</div>", article).group(2)
+    page["contents"] = re.findall(r"<p>([^<>]+?)</p>", body)
+    del body
+  except:
+    print("\terror crawl body!\n url:", url)
   return page
 
--- index.py
+++ index.py
@@ -8,7 +8,9 @@ namaFileCorpus = "corpus"
 
 page = int(input("Jumlah page >> "))
 
+print("crawling katalog...")
 articles = katalog_crawler.crawl(URL_KATALOG, page)
+print("katalog length:", len(articles))
 
 with open(f"hasil/{namaFileKatalog}.json", 'w') as f:
   json.dump(articles, f)
@@ -16,7 +18,7 @@ with open(f"hasil/{namaFileKatalog}.json", 'w') as f:
 corpus = []
 for index, article in enumerate(articles):
   corpus.append(artikel_crawler.crawl(article["link"]))
-  print(f"{index}. crawling article...")
+  print(f"{index}.\tcrawling article...")
 
 with open(f"hasil/{namaFileCorpus}.json", 'w') as f:
   json.dump(corpus, f)
